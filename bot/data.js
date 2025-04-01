1c76da0b4a6380c80e629a19b9558b749437ff1f
whizshop <treyblair585@gmail.com>
2025-04-01 18:06:50 +0300

    Create data.js

---
+++ bot/data.js
@@ -0,0 +1,5 @@
+module.exports = {
+  token: "7682210283:AAHAcAmChXMsGYmoeO8Z5n7lh76o-UP0vr4",
+  adminId: 7612857358,  // Your ID for admin bypass
+  requiredChannel: "@whiz_t" // Channel users must join
+};
